539f0d707b94d66860e4ba1b36faff75a0857395
Megumi-0712 <toruansya@yahoo.co.jp>
2020-05-16 14:36:18 +0900

    Error kaiketu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "homes#top"
-  get "books/index" => "books#index"
-  get "books/show" => "books#show"
-  get "books/edit" => "books#edit"
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
